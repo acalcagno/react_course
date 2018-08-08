0ef9d9967c3bb2d9faf5d42a6440a48bc22c4739
acalcagno <calqui@gmail.com>
2018-08-08 12:36:21 -0300

    functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-
-class Dishdetail extends Component {
-
-    renderDish(dish) {
+    function RenderDish({dish}) {
         return( <div className="col-12 col-sm-5 m-1">
             <Card>
-                <CardImg width="100%" src={dish.image} alt={this.props.dish.name}/>
+                <CardImg width="100%" src={dish.image} alt={dish.name}/>
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
@@ -16,36 +13,35 @@ class Dishdetail extends Component {
         </div>)
     }
 
-
-    renderComments(comments) {
-        if(this.props.dish) {
-        return(
-            comments.map((each) => {
-                return (
-                    <li key={each.id}>
-                        <div className="blockquote">{each.comment}</div>
-                        <div className="blockquote-footer"> {each.author}, { new Intl.DateTimeFormat('en-US', { year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(each.date)))}</div>
-                        <br/>
-                    </li>
-                )
-            })
-        )} else {return(<div></div>)}
+    function RenderComments({comments}) {
+        if(comments) {
+            return( <div className="col-12 col-sm-5 m-1">
+                <div className="h4">Comments</div>
+                    <ul className="list-unstyled">
+                        {comments.map((each) => {
+                            return (
+                                <li key={each.id}>
+                                    <div className="blockquote">{each.comment}</div>
+                                    <div className="blockquote-footer"> {each.author}, { new Intl.DateTimeFormat('en-US', { year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(each.date)))}</div>
+                                    <br/>
+                                </li>
+                            )})}
+                    </ul>
+                </div>
+            )
+        } else {
+            return(<div></div>)
+        }
     }
 
-    render() {
+    const Dishdetail = (props) => {
 
-        if (this.props.dish != null) {
+        if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderDish(this.props.dish)}
-
-                        <div className="col-12 col-sm-5 m-1">
-                            <div className="h4">Comments</div>
-                            <ul className="list-unstyled">
-                                {this.renderComments(this.props.dish.comments)}
-                            </ul>
-                        </div>
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={ props.dish.comments } />
                     </div>
                 </div>
             )
@@ -56,7 +52,7 @@ class Dishdetail extends Component {
             )
         }
     }
-}
+
 
 
 export default Dishdetail;
