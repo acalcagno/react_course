a90c7b154b01c6f3279181b0ad31ff732baf3eef
acalcagno <calqui@gmail.com>
2018-08-12 22:44:25 -0300

    fix CommentForm separado en otro archivo

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,105 +1,10 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
-    Button, Modal, ModalHeader, ModalBody, Label, Col, Row } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom'
-import { Control, LocalForm, Errors } from 'react-redux-form'
 import { Loading } from './LoadingComponent'
+import CommentForm from './CommentForm'
 
 
-const maxLenght = (len) => (val) => !(val) || (val.length <= len)
-const minLength = (len) => (val) => (val) && (val.length > len)
-
-class CommentForm extends Component {
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            isModalOpen: false
-        }
-
-        this.toggleModal = this.toggleModal.bind(this)
-    }
-
-    toggleModal() {
-        this.setState( {
-            isModalOpen: !this.state.isModalOpen
-        })
-    }
-
-    handleSubmit(values){
-        this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <Button outline color="info" onClick={this.toggleModal} >
-                    <span className="fa fa-pencil" /> Submit Comment
-                </Button>
-                <Modal isOpen = {this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login
-                    </ModalHeader>
-                    <ModalBody>
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                            <Row className="form-group">
-                                <Col>
-                                    <Label htmlFor="rating">Rating</Label>
-                                    <Control.select model=".rating" name="rating"
-                                                    className="form-control"
-                                                    innerRef={(input) => this.rating = input }>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                    </Control.select>
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col>
-                                    <Label htmlFor="author" >Your Name</Label>
-                                    <Control.text className="form-control"
-                                                  model=".author" id="author" name="author" placeHolder="Your Name"
-                                                  innerRef={(input) => this.author = input }
-                                                  validators={{
-                                                      minLength: minLength(2), maxLength: maxLenght(15)
-                                                  }}/>
-                                    <Errors
-                                        className="text-danger"
-                                        model=".author"
-                                        show="touched"
-                                        messages={{
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }} />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col>
-                                    <Label htmlFor="comment">Comment</Label>
-                                    <Control.textarea model=".comment" id="comment" name="comment"
-                                                      rows="6"
-                                                      className="form-control"
-                                                      innerRef={(input) => this.comment = input }/>
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col>
-                                    <Button type="submit" color="primary">
-                                        Submit
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </LocalForm>
-                    </ModalBody>
-                </Modal>
-            </React.Fragment>
-        )
-    }
-}
 
 
 
